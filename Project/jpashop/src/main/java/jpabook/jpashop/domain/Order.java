c72fb0877942608634c9822655400292e1a96b4d
plitche <kysu728@naver.com>
2022-03-26 18:34:41 +0900

    도메인 분석 설계 - 엔티티 설계시 주의점

--- Project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -18,19 +20,45 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    // orderItems에 data를 넣어두고 order를 저장하면 같이 저장된다. 원래는 order에 세팅해야 할텐데..
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
+    // 스프링 부트 신규 설정 (엔티티(필드) -> 테이블(컬럼)) 카멜 케이스 -> 언더스코어(order_date)
     private LocalDateTime orderDate; // 주문시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCLE]
+
+    // == 연관관계 편의 메서드 ==
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+/*
+    public static void main(String[] args) {
+        Member member = new Member();
+        Order order = new Order();
+        order.setMember(member);
+    }
+*/
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
