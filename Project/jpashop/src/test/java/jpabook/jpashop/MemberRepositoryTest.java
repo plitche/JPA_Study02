3494e78da09579c436cb69d821b504a44d56e988
plitche <kysu728@naver.com>
2022-03-26 17:24:09 +0900

    도메인 분석 설계 - 엔티티 클래스 개발1

--- Project/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ Project/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,17 +1,19 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.MemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 public class MemberRepositoryTest {
 
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     @Transactional
