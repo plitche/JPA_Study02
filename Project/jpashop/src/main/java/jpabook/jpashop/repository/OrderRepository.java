6e13cb68494b900d2e0cdb7272e1488e8c44548c
plitche <kysu728@naver.com>
2022-05-19 23:58:00 +0900

    8. API 개발 고급(지연 로딩과 조회 서능 최적화) - 간단한 주문 조회V1 엔티티를 DTO로 변환 - 페치 조인 최적화

--- Project/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Project/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -89,5 +89,12 @@ public class OrderRepository {
          return orderTypedQuery.getResultList();
      }
 
-     
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class
+        ).getResultList();
+    }
 }
